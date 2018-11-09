9c5812064323646c501d03e74893b69e6b225a88
PlayMa256 <matheus.g.silva@hotmail.com>
2018-11-09 10:14:49 -0200

    feat(web): add web package

---
+++ packages/web/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	notify: false,
+	bail: false,
+	snapshotSerializers: ['jest-serializer-html'],
+  displayName: 'web-app',
+	setupTestFrameworkScriptFile: require.resolve('./jest.setup.js'),
+	transformIgnorePatterns: ['node_modules/'],
+	coverageReporters: ['lcov', 'html'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+	},
+	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.js?$',
+	moduleNameMapper: {
+		'.(css)$': '<rootDir>/test_utils/stub.js',
+		'.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/test_utils/stub.js',
+		'.(woff|woff2|otf|ttf|eot|csv)$':
+			'<rootDir>/test_utils/stub.jss'
+	},
+};
---
+++ packages/web/webpack.config.js
@@ -0,0 +1,114 @@
+const { resolve } = require('path');
+
+const webpack = require('webpack');
+const { WebpackPluginServe: Serve } = require('webpack-plugin-serve');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const outputPath = resolve(__dirname, 'dist');
+
+const entry = isDev ? ['./src/index.js', 'webpack-plugin-serve/client'] : './src/index.js';
+
+const plugins = [
+  new HtmlWebpackPlugin(),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+    }
+  })
+];
+
+if (isDev) {
+  plugins.push(new Serve({
+    hmr: true,
+    historyFallback: true,
+    static: [outputPath]
+  }));
+} else {
+  plugins.push(new MiniCssExtractPlugin())
+}
+
+module.exports = {
+  entry,
+  mode: process.env.NODE_ENV,
+  devtool: 'cheap-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: !isDev ? [MiniCssExtractPlugin.loader, 'css-loader'] : ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.woff(\?.*)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: 'fonts/[name].[ext]',
+            mimetype: 'application/font-woff'
+          }
+        }
+      },
+      {
+        test: /\.woff2(\?.*)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: 'fonts/[name].[ext]',
+            mimetype: 'application/font-woff2'
+          }
+        }
+      },
+      {
+        test: /\.(otf)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'fonts/[name].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.ttf(\?.*)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: 'fonts/[name].[ext]',
+            mimetype: 'application/octet-stream'
+          }
+        }
+      },
+      {
+        test: /\.svg(\?.*)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: 'images/[name].[ext]',
+            mimetype: 'image/svg+xml'
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg)(\?.*)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: 'images/[name].[ext]'
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    path: outputPath,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins,
+  watch: isDev
+};
