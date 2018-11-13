e2c3b3091b32dade06a41dbeab590e99be62f27c
vikr01 <vik120@icloud.com>
2018-11-13 00:27:28 -0800

    fix app package's jest config

--- babel.config.js
+++ babel.config.js
@@ -1,40 +1,5 @@
 const { workspaces = [] } = require('./package.json');
 
 module.exports = {
-  babelrcRoots: workspaces.packages || workspaces,
-  presets: [
-    '@babel/preset-flow',
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-  ],
-  plugins: [
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-export-default-from',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-transform-async-to-generator',
-    '@babel/plugin-syntax-async-generators',
-  ],
-  overrides: [
-    {
-      test: ['./packages/**/*.ts'],
-      presets: [
-        '@babel/preset-typescript',
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              node: 'current',
-            },
-          },
-        ],
-      ],
-    },
-  ],
+  babelrcRoots: workspaces.packages || workspaces
 };
