bd7c67b6b0fcb83cdb111108e5c210b08cc091b6
PlayMa256 <matheus.g.silva@hotmail.com>
2018-11-17 23:32:41 -0200

    misc: add web to jest projects

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   projects: [
     '<rootDir>/packages/app/jest.config.js',
-    '<rootDir>/packages/server/jest.config.js'
+    '<rootDir>/packages/server/jest.config.js',
+    '<rootDir>/packages/web/jest.config.js'
   ],
   transform: {
     '^.+\\.(js|ts|tsx)?$': require.resolve('babel-jest'),
