8526b199cc0ddb98a2bdd7dc0098da49fe84502e
Paulo Eduardo Rezende <rezendepauloedu@outlook.com>
2018-11-22 13:18:47 -0200

    fix: change path to .env file

--- packages/server/src/config.js
+++ packages/server/src/config.js
@@ -1,12 +1,10 @@
 // @flow
-import path from 'path';
+import { resolve } from 'path';
 import dotenvSafe from 'dotenv-safe';
 
-const root = path.join.bind(this, __dirname, '../');
-
 dotenvSafe.load({
-  path: root('.env'),
-  sample: root('.env.example'),
+  path: resolve('.env'),
+  sample: resolve('.env.example'),
 });
 
 const ENV = ((process.env: any): {
