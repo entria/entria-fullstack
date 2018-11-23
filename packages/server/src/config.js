ab452cf7223fc6d520f3e8a546fc53c4a371af5a
Paulo Eduardo Rezende <rezendepauloedu@outlook.com>
2018-11-23 00:58:29 -0200

    refactor: revert commit #8526b19

--- packages/server/src/config.js
+++ packages/server/src/config.js
@@ -1,10 +1,12 @@
 // @flow
-import { resolve } from 'path';
+import path from 'path';
 import dotenvSafe from 'dotenv-safe';
 
+const root = path.join.bind(this, __dirname, '../');
+
 dotenvSafe.load({
-  path: resolve('.env'),
-  sample: resolve('.env.example'),
+  path: root('.env'),
+  sample: root('.env.example'),
 });
 
 const ENV = ((process.env: any): {
