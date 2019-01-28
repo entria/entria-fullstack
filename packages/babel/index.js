c7fb6f8b22bc4a80aee18f76487bb7509df9956d
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2019-01-28 21:59:24 -0200

    feat(babel): improve babel with relay (#60)

--- packages/babel/index.js
+++ packages/babel/index.js
@@ -1,23 +1,40 @@
 module.exports = {
   presets: [
-    '@babel/preset-flow',
+    '@babel/preset-react',
     [
       '@babel/preset-env',
       {
-        targets: {
-          node: 'current',
-        },
+        modules: false,
       },
     ],
-    '@babel/preset-react',
     '@babel/preset-typescript',
   ],
   plugins: [
+    'react-hot-loader/babel',
+    'babel-plugin-styled-components',
+    [
+      'relay',
+      {
+        schema: '../server/data/schema.json',
+      },
+    ],
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-transform-async-to-generator',
-    '@babel/plugin-syntax-async-generators',
   ],
+  env: {
+    test: {
+      presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+      plugins: [
+        '@babel/plugin-transform-runtime',
+        'dynamic-import-node',
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-proposal-object-rest-spread',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-export-default-from',
+        '@babel/plugin-proposal-export-namespace-from',
+      ],
+    },
+  },
 };
