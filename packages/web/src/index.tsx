8e4a2ba7b0348b1475a174b6babf01a7062f53cf
PlayMa256 <matheus.g.silva@hotmail.com>
2019-05-16 11:19:47 -0300

    fix: target container not being available

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -4,4 +4,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.createElement('div');
+document.body.appendChild(root);
+
+ReactDOM.render(<App />, root);
