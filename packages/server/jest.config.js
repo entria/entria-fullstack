9917841d1197a8a95b794fe07a52cb67f42eaa00
AugustoCalaca <augustocalaca2@gmail.com>
2019-10-28 23:54:21 -0300

    chore(web): remove comments

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,20 +1,10 @@
 const pack = require('./package');
 
-// const ignoredPaths = [
-//   '<rootDir>/node_modules/',
-//   '<rootDir>/dist',
-//   '<rootDir>/scripts',
-//   '<rootDir>/repl/',
-//   '<rootDir>/flow-typed/',
-//   '<rootDir>/test/',
-// ];
-
 module.exports = {
   displayName: pack.name,
   name: pack.name,
   testEnvironment: '<rootDir>/test/environment/mongodb',
-  // testPathIgnorePatterns: ignoredPaths,
-  // coverageReporters: ['lcov', 'html'],
+  testPathIgnorePatterns: ['/node_modules/', './dist'],
   setupFilesAfterEnv: ['<rootDir>/test/setupTestFramework.js'],
   globalSetup: '<rootDir>/test/setup.js',
   globalTeardown: '<rootDir>/test/teardown.js',
@@ -30,9 +20,6 @@ module.exports = {
     ],
   ],
   transform: {
-    // '^.+\\.(js|ts|tsx)?$': 'babel-jest',
-    // '^.+\\.(js|ts|tsx)?$': '<rootDir>/node_modules/babel-jest',
-    // '^.+\\.(js|ts|tsx)?$': '<rootDir>/../../node_modules/babel-jest',
     '^.+\\.(js|ts|tsx)?$': '<rootDir>/test/babel-transformer',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?)$',
