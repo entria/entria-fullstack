bc9808ca7edf3e72c4583ee294525216997e4f7e
AugustoCalaca <augustocalaca2@gmail.com>
2019-10-28 23:53:20 -0300

    chore(web): remove jest config web

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   projects: [
     '<rootDir>/packages/app/jest.config.js',
     '<rootDir>/packages/server/jest.config.js',
-    '<rootDir>/packages/web/jest.config.js'
   ],
   transform: {
     '^.+\\.(js|ts|tsx)?$': require('path').resolve('./customBabelTransformer'),
