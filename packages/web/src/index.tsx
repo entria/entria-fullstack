f556c72e677870ffe162d15c43d3d9142e1884f9
thiagoleitedev <thiagoleitebr@gmail.com>
2019-11-20 15:47:26 -0300

    Prettier

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,5 +1,5 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
