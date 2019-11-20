5ccc12cb249ada1167810499116602ce092e8e18
thiagoleitedev <thiagoleitebr@gmail.com>
2019-11-20 15:48:18 -0300

    Use suspense with relay provider

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { RelayEnvironmentProvider } from 'react-relay/hooks';
+import RelayEnvironment from './relay/Environment';
+import ErrorBoundary from './ErrorBoundary';
 import { hot } from 'react-hot-loader';
 
-import UserList from './UserList';
+const UserList = React.lazy(() => import('./UserList'));
+
+const { Suspense } = React;
 
 function App() {
-  return <UserList />;
+  return (
+    <RelayEnvironmentProvider environment={RelayEnvironment}>
+      <ErrorBoundary>
+        <Suspense fallback={'Loading...'}>
+          <UserList />
+        </Suspense>
+      </ErrorBoundary>
+    </RelayEnvironmentProvider>
+  );
 }
 
 export default hot(module)(App);
