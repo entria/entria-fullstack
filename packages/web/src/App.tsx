3d76f9fe03927a79283c07e201150d7f7c1248fc
thiagoleitedev <thiagoleitebr@gmail.com>
2019-11-20 16:13:19 -0300

    Import Suspense directly from react package

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { RelayEnvironmentProvider } from 'react-relay/hooks';
 import RelayEnvironment from './relay/Environment';
 import ErrorBoundary from './ErrorBoundary';
 import { hot } from 'react-hot-loader';
 
 const UserList = React.lazy(() => import('./UserList'));
-
-const { Suspense } = React;
 
 function App() {
   return (
