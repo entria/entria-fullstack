5b57dcea7355f80941318e7b811b3dbcf2261a6f
thiagoleitedev <thiagoleitebr@gmail.com>
2019-11-20 15:46:30 -0300

    Add ErrorBoundary Component

---
+++ packages/web/src/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+type Props = {
+  children: React.ReactNode;
+};
+type State = any;
+
+export default class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { error: null };
+  }
+
+  static getDerivedStateFromError(error: any) {
+    return {
+      error,
+    };
+  }
+
+  render() {
+    if (this.state.error != null) {
+      return (
+        <div>
+          <div>Error: {this.state.error.message}</div>
+          <div>
+            <pre>{JSON.stringify(this.state.error.source, null, 2)}</pre>
+          </div>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
